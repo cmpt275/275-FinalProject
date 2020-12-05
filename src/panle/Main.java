0f5cd5f70155a30b64942e9df3ada3b80c1b5910
Haoming <haomingjing@gmail.com>
2020-12-04 22:25:44 -0800

    normal update

--- src/panle/Main.java
+++ src/panle/Main.java
@@ -19,40 +19,42 @@ public class Main {
         // write your code here
         //CalendarProgram mode =new CalendarProgram();
         Events events= new Events();
-        try {
-            /////////////weather////////////////////////
-            String publicIP = getIP();
-            Location loc = getLatLong(publicIP);
-            List<FiveDaysForecast> wlist = getNextFiveDayWeather(loc.getLatitude(), loc.getLongitude());
-            int c = 0;
-            String[] d = {"Today", "Tomorrow", "NextDay", "NextDay", "NextDay"};
-            for (FiveDaysForecast ls : wlist) {
-                String temp = ls.getTemp();
-                String hum = ls.getRelative_humidity();
-                Image img = ls.getIconImage();
-                ImageIcon icon = new ImageIcon(img);
-                Weather weather = ls.getWeather();
-                String description = weather.getDescription();
+        if(false) {
+            try {
+                /////////////weather////////////////////////
+                String publicIP = getIP();
+                Location loc = getLatLong(publicIP);
+                List<FiveDaysForecast> wlist = getNextFiveDayWeather(loc.getLatitude(), loc.getLongitude());
+                int c = 0;
+                String[] d = {"Today", "Tomorrow", "NextDay", "NextDay", "NextDay"};
+                for (FiveDaysForecast ls : wlist) {
+                    String temp = ls.getTemp();
+                    String hum = ls.getRelative_humidity();
+                    Image img = ls.getIconImage();
+                    ImageIcon icon = new ImageIcon(img);
+                    Weather weather = ls.getWeather();
+                    String description = weather.getDescription();
 
 
-                JLabel day1 = new JLabel();
+                    JLabel day1 = new JLabel();
 
+                    events.weatherP.add(day1);
+                    day1.setBounds(0 + 252 * c, 0, 251, 85);
+                    day1.setOpaque(true);
+                    day1.setBackground(Color.white);
+                    day1.setBorder(BorderFactory.createTitledBorder("<html><font color=blue size=4><h>" + d[c] + "</h></html>\""));
+                    day1.setText("<html><font color=black size=3><body><p><b>Temp:" + temp + "° C</b></p>\n<p><b>Humidity:" + hum + "%</b></p>\n<p><b>" + description + "</b></p></body></html>\"");
+                    day1.setIcon(icon);
+
+                    c++;
+                }
+                ////////////end weather////////////////////
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+                JLabel day1 = new JLabel("<html><font color=red size=4><body><p><b>" + "No Internet" + "</b></p><p>Cannot Load Weather Information</p></body></html>\"");
                 events.weatherP.add(day1);
-                day1.setBounds(0 + 252 * c, 0, 251, 85);
-                day1.setOpaque(true);
-                day1.setBackground(Color.white);
-                day1.setBorder(BorderFactory.createTitledBorder("<html><font color=blue size=4><h>" + d[c] + "</h></html>\""));
-                day1.setText("<html><font color=black size=3><body><p><b>Temp:" + temp + "° C</b></p>\n<p>Humidity:" + hum + "%</p>\n<p><b>" + description + "</b></p></body></html>\"");
-                day1.setIcon(icon);
-
-                c++;
+                day1.setBounds(20, 0, 251, 85);
             }
-            ////////////end weather////////////////////
-        }catch (IOException ioException) {
-            ioException.printStackTrace();
-            JLabel day1 = new JLabel("<html><font color=red size=4><body><p><b>"+"No Internet"+"</b></p><p>Cannot Load Weather Information</p></body></html>\"");
-            events.weatherP.add(day1);
-            day1.setBounds(20, 0, 251, 85);
         }
         //  getCurrentWeatherByLatLong("49.22830624640323", "-122.9998086267361");
         //   getNextFiveDaysWeather("49.22830624640323", "-122.9998086267361");
